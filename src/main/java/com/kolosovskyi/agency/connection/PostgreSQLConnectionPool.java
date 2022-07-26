2972961ae81304a9a5fcec54b19fd72c68be4e03
Andrii Kolosovskyi <kolosovskyiandr@gmail.com>
2022-07-26 19:33:39 +0300

    complete logging 1.0 (26.07.2022)

--- src/main/java/com/kolosovskyi/agency/connection/PostgreSQLConnectionPool.java
+++ src/main/java/com/kolosovskyi/agency/connection/PostgreSQLConnectionPool.java
@@ -1,21 +1,34 @@
 package com.kolosovskyi.agency.connection;
 
-import com.kolosovskyi.agency.Main;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class PostgreSQLConnectionPool {
-    private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(PostgreSQLConnectionPool.class);
     private final HikariDataSource dataSource;
-
     private PostgreSQLConnectionPool() {
-        HikariConfig config = new HikariConfig("src/main/resources/hikari.properties");
+        HikariConfig config = new HikariConfig(readProperties());
         dataSource = new HikariDataSource(config);
+    }
+    private static Properties readProperties() {
+        Properties properties;
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        try (InputStream resource = loader.getResourceAsStream("hikari.properties")) {
+            properties = new Properties();
+            properties.load(resource);
+        } catch (IOException e) {
+            LOGGER.error("Can not read properties for pool", e);
+            properties = new Properties();
+        }
+        return properties;
     }
 
     private static class ConnectionPoolHolder {
@@ -27,7 +40,7 @@ public class PostgreSQLConnectionPool {
     }
 
     public Connection getConnection() throws SQLException {
-        logger.info("GETTING CONNECTION");
+        LOGGER.info("GETTING CONNECTION");
         return dataSource.getConnection();
     }
 }
